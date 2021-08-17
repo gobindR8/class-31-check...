6f8461d4764d78d41766b87ffe59a8458f029926
gobindR8 <74371640+gobindR8@users.noreply.github.com>
2021-08-17 13:43:52 +0530

    Add files via upload

---
+++ droplet.js
@@ -0,0 +1,41 @@
+class droplet{
+constructor(x,y,r){
+var options={
+  isStatic:false,
+   restitution:0.1,
+   desnity:1,
+   friction:1
+   
+
+}
+this.x=x
+this.y=y
+this.r=r
+this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+World.add(world,this.body)
+}
+display(){
+var pos=this.body.position
+var angle=this.body.angle
+push()
+translate(pos.x,pos.y)
+rotate(angle)
+strokeWeight(4)
+fill(100,0,255)
+rectMode(CENTER)
+ellipse(0,0,this.r,this.r)
+pop()
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
